b83fc34f3c284037a5b2cf94e153b41876a80037
Tarun7937 <68948001+Tarun7937@users.noreply.github.com>
2020-11-11 17:34:58 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,9 +12,9 @@ function preload()
   //load images here
   dogimg1 = loadImage("images/dogImg.png")
   dogimg2 = loadImage("images/dogImg1.png")
-  bedroom = loadImage("images/Living Room.png");
+  bedroom = loadImage("images/LivingRoom.png");
   garden = loadImage("images/Garden.png");
-  washroom= loadImage("images/Wash Room.png");
+  washroom= loadImage("images/WashRoom.png");
 
 }
 
